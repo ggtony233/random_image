04fcd1d4b3b9ab150a28032ec366e8b62564e4cd
1923113775@qq.com <1923113775@qq.com>
2025-04-22 06:16:45 +0800

    修改copy

--- process/randimage.go
+++ process/randimage.go
@@ -1,6 +1,9 @@
 package process
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"random_image/utils"
@@ -13,7 +16,14 @@ func RandomImage(w http.ResponseWriter, r *http.Request) {
 	imageData, contentType := utils.GetFile()
 	log.Printf("一次请求，图片类型：%s", contentType)
 	//http.ServeFile(w, r, ImagePath)
-
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(imageData)))
 	w.Header().Set("Content-Type", contentType)
-	w.Write(imageData)
+	w.Header().Set("Cache-Control", "public, max-age=86400")
+	_, err := io.Copy(w, bytes.NewReader(imageData))
+	if err != nil {
+		log.Printf("%s", err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
 }
