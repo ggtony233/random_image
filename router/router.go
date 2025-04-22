3babfbfd1a5d3196548b768f5b421a149636f632
1923113775@qq.com <1923113775@qq.com>
2025-04-22 08:16:58 +0800

    添加路由

--- router/router.go
+++ router/router.go
@@ -13,6 +13,11 @@ func Router() {
 	utils.StartAutoRefresh(10 * time.Minute)
 	utils.RefreshFilelist(10 * time.Hour)
 	http.HandleFunc("/", process.RandomImage)
+	//	http.HandleFunc("/refresh", process.Refresh)
+	http.HandleFunc("/dl", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Disposition", "attachment; filename="+utils.Name)
+		process.RandomImage(w, r)
+	})
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
