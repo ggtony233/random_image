9e62957e1d62db124aeed355a8df47f79e03c1b1
1923113775@qq.com <1923113775@qq.com>
2025-04-25 22:12:24 +0800

    modified:   utils/getrandompath_test.go     增加错误判定

--- utils/getrandompath_test.go
+++ utils/getrandompath_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"encoding/json"
+	"os"
 	"testing"
 )
 
@@ -10,4 +12,20 @@ func TestGenFileName(t *testing.T) {
 	if filename != "eg.xxx" {
 		t.Error("GenFileName error")
 	}
+}
+func TestReadOneFile(t *testing.T) {
+	jsondata, _ := os.ReadFile(GetJsonPath())
+	FileList := TrueFilelist{
+		RootPath: "",
+		Files:    []Myfile{},
+	}
+	json.Unmarshal(jsondata, &FileList)
+	FileList.Files = []Myfile{}
+	data, _ := json.Marshal(FileList)
+	os.WriteFile(GetJsonPath(), data, 0644)
+	err := ReadOneFile()
+	if err != nil {
+		t.Error(err)
+	}
+
 }
